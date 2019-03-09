64d6b3a894014ba0aa56d1317657da27854bcabd
Stanley-Okwii <stanleeparker12@gmail.com>
2019-03-09 06:06:18 +0300

    feat (articles): create, read, update and delete articles
    - Add CRUD functionality articles
    - Clean up redundant code
    
    [Delivers #164069312]

--- authors/apps/articles/renderers.py
+++ authors/apps/articles/renderers.py
@@ -1,51 +1,14 @@
 
-import json
-
-from rest_framework.renderers import JSONRenderer
-from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
+from ..renderers import MainRenderer
 
 
-class CategoryJSONRenderer(JSONRenderer):
-    charset = 'utf-8'
-
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        """
-        render response data
-        :param data:
-        :param accepted_media_type:
-        :param renderer_context:
-        :return:
-        """
-        if type(data) != ReturnList:
-            errors = data.get('errors', None)
-            if errors is not None:
-                return super(CategoryJSONRenderer, self).render(data)
-
-        if type(data) == ReturnDict:
-            return json.dumps({
-                'category': data
-            })
-
-        else:
-            return json.dumps({
-                'categories': data
-            })
+class CategoryJSONRenderer(MainRenderer):
+    object_label = 'category'
 
 
-class ArticleJSONRenderer(JSONRenderer):
-    charset = 'utf-8'
+class ArticleJSONRenderer(MainRenderer):
+    object_label = 'article'
 
-    def render(self, data, media_type=None, renderer_context=None):
-        if(type(data) != ReturnList):
-            errors = data.get('errors', None)
-            if errors is not None:
-                return super(ArticleJSONRenderer, self).render(data)
 
-        if type(data) == ReturnDict:
-            return json.dumps({
-                'article': data
-            })
-
-        return json.dumps({
-            'articles': data
-        })
+class BookmarkJSONRenderer(MainRenderer):
+    object_label = 'bookmark'
--- authors/apps/profiles/renderers.py
+++ authors/apps/profiles/renderers.py
@@ -1,16 +1,5 @@
-import json
-from rest_framework.renderers import JSONRenderer
+from ..renderers import MainRenderer
 
 
-class ProfileJSONRenderer(JSONRenderer):
-    charset = 'utf-8'
-
-    def render(self, data, media_type=None, renderer_context=None):
-        errors = data.get('errors', None)
-
-        if errors is not None:
-            return super(ProfileJSONRenderer, self).render(data)
-
-        return json.dumps({
-            'profile': data
-        })
+class ProfileJSONRenderer(MainRenderer):
+    object_label = 'profile'
